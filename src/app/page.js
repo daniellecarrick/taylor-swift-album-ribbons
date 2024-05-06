14740a5c2f9cdfe57d716b8467f30cce8b6677f9
Danielle Carrick <daniellecarrick@Danielles-MacBook-Pro.local>
2024-05-06 09:24:38 -0400

    copy updates

--- src/app/page.js
+++ src/app/page.js
@@ -48,9 +48,10 @@ export default function Home() {
         />
         <p>
           Taylor Swift famously shifts her sound with each album, creating new
-          &quot;eras&quot; in her discography. From her twangy country origins in{" "}
-          <i>Taylor Swift</i> to the indie-infused narratives of <i>folklore</i>{" "}
-          and <i>evermore</i>, Swift has continuously reinvented her sound.
+          &quot;eras&quot; in her discography. From her twangy country origins
+          in <i>Taylor Swift</i> to the indie-infused narratives of{" "}
+          <i>folklore</i> and <i>evermore</i>, Swift has continuously reinvented
+          her sound.
         </p>
         <p>
           Sonically, we recognize these distinct eras in her discography…but can
@@ -63,15 +64,16 @@ export default function Home() {
             Spotify API
           </a>{" "}
           provides a set of audio features for every song that we can use to
-          understand trends through Taylor Swift's discography.
-          <p>In particular, I use four of Spotify's measures:</p>
-          <ul>
-            <li>Energy</li>
-            <li>Danceability</li>
-            <li>Acousticness </li>
-            <li>Loudness</li>
-          </ul>
+          understand trends through Taylor Swift's discography.{" "}
         </p>
+        <p>In particular, I use four of Spotify's measures:</p>
+        <ul className="list-disc ml-8">
+          <li>Energy</li>
+          <li>Danceability</li>
+          <li>Acousticness </li>
+          <li>Loudness</li>
+        </ul>
+
         <p>
           To visualize a song, I convert these measures to RGBA space. That
           gives me a color swatch for the song. String all the songs in an album
@@ -89,9 +91,9 @@ export default function Home() {
         <h3>More energetic songs appear redder</h3>
         <p>
           Energetic songs are characterized by intensity and vigor. Taylor
-          Swift's most energetic song is &quot;Haunted&quot; off of <i>Speak Now</i>. It
-          pulses with emotion and intensity, and presents in the analysis as a
-          vividly warm red.
+          Swift's most energetic song is &quot;Haunted&quot; off of{" "}
+          <i>Speak Now</i>. It pulses with emotion and intensity, and presents
+          in the analysis as a vividly warm red.
         </p>
         <div className="flex flex-row gap-4">
           <Player
@@ -112,10 +114,9 @@ export default function Home() {
         <h3>More danceable songs appear green</h3>
         <p>
           Danceability measures musical elements like tempo, rhythm stability,
-          beat strength, and overall regularity. &quot;I Think He Knows&quot; off of{" "}
-          <i>Lover</i>
-          has one of the highest danceability scores, and and so its
-          corresponding color swatch is a sugary lime green:
+          beat strength, and overall regularity. &quot;I Think He Knows&quot;
+          off of <i>Lover</i> has one of the highest danceability scores, and
+          and so its corresponding color swatch is a sugary lime green.
         </p>
         <div className="flex flex-row gap-4">
           <Player
@@ -137,15 +138,16 @@ export default function Home() {
         <h3>More acoustic songs appear blue</h3>
         <p>
           Songs with high acoustic-ness will have stripped-down arrangements and
-          organic instrumentation. &quot;Sweet Nothing&quot; off <i>Midnights</i> is a
-          nice example of a highly acoustic song. Look at its swatch here:
+          organic instrumentation. &quot;Sweet Nothing&quot; off{" "}
+          <i>Midnights</i> is a nice example of a highly acoustic song. Look at
+          its swatch here:
         </p>
         <div className="flex flex-row gap-4">
           <Player
             preview_url={
               "https://p.scdn.co/mp3-preview/5d55bc6094ad5ca89529ee2b0cbee31daa3e1a1c?cid=cfe923b2d660439caf2b557b21f31221"
             }
-            album={"Midnights (3am edition)"}
+            album={"Midnights (The Til Dawn Edition)"}
             track={"Sweet Nothing"}
             r={rScale(0.166)}
             g={gScale(0.335)}
@@ -154,10 +156,9 @@ export default function Home() {
           />
         </div>
         <p>
-          I know you might be thinking &quot;Hey, that looks pretty purple&quot;. And
-          you're right! That's because while it ranks high on acousticness, it
-          has a low loudness value (making it lighter). Because it's a softer
-          song, it's opacity is set at 28. Here{" "}
+          I know you might be thinking &quot;Hey, that looks pretty
+          purple&quot;. And you're right! That's because while it ranks high on
+          acousticness, it has a low loudness value, making it lighter. Here{" "}
           <span
             style={{
               height: 12,
@@ -166,25 +167,36 @@ export default function Home() {
               display: "inline-block",
             }}
           ></span>{" "}
-          is what it would look like without opacity applied. Now THAT's blue!
+          is what it would look like without opacity applied. Now <b>that's</b>{" "}
+          blue!
         </p>
 
         <h3>Things to look for in the Era's Paint Swatchs</h3>
         <p>
           As you explore all of the albums below, there are a few things to pay
           attention to. First of all, you'll notice that <i>folklore</i> and{" "}
-          <i>evermore</i>, considered part of the same era, have color swatches
-          composed of light purple with a sprinkle of blues and pinks. Those two
-          albums represent Swift's journey into indie influences and showcase
-          some of her most acoustic songs.
+          <i>evermore</i>, considered part of the same era, are also visually
+          similar. They both have color swatches composed of light purple with a
+          sprinkle of blues and pinks. Those two albums represent Swift's
+          journey into indie influences and showcase some of her most acoustic
+          songs.
         </p>
-        <p>You'll notice that her earlier work ...</p>
+        <p>
+          In fact, he last 4 albums have been the most acoustic of her career.
+          Earlier albums have higher median danceability and energy values. In
+          fact, <i>1989 (Taylor's Version)</i> and{" "}
+          <i>Speak Now (Taylor's Version)</i> both have the highest median
+          energy value and you can see that in their color ribbon's stronger red
+          tones. <i>Lover</i> has the highest median danceability and therefore
+          the most green tones.
+        </p>
         <p>
           Of course this approach is subject to interpretation. Using different
           audio features or assigning them to different colors will produce
-          different color ribbons. I don't think we can truly define a song by 4
-          metrics and a color. But it's cool to see how these values illuminate
-          some elements of Swift's work.
+          different color ribbons and highlight different sounds. I don't think
+          we can truly define a song with four metrics and a color. But even
+          these few features quickly illuminate the musical quality of Swift's
+          work.
         </p>
       </div>
       <div className="viz z-10 w-full items-center">
